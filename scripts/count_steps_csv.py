b4678cf2d8429c8040c9fdeb68c4a922dbc932dd
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2025-11-14 17:12:29 -0600

    also measure routing footprint

--- scripts/count_steps_csv.py
+++ scripts/count_steps_csv.py
@@ -3,6 +3,7 @@ import subprocess
 import json
 import argparse
 import statistics
+from collections import defaultdict
 from concurrent.futures import ProcessPoolExecutor, as_completed
 import csv
 
@@ -24,32 +25,32 @@ os.makedirs(bench_output_dir, exist_ok=True)
 
 # === configurations ===
 HBM_CASES = [
-    # name                                      config                                       extra args
-    ("no_hbm",                                  "no_hbm",                                   ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("no_hbm_single_magic_state",               "no_hbm_single_magic_state",                ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("shared_none",                             "shared_none",                              ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("shared_2-route_bottom",                   "shared_2-route_bottom",                    ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("shared_2-route_upper",                    "shared_2-route_upper",                     ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_4-route_bottom",                   "shared_4-route_bottom",                    ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_4-route_upper",                    "shared_4-route_upper",                     ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("shared_none-anchilla_perimeter",          "shared_none-anchilla_perimeter",           ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("shared_2-route_bottom-anchilla_perimeter","shared_2-route_bottom-anchilla_perimeter", ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    ("shared_2-route_upper-anchilla_perimeter", "shared_2-route_upper-anchilla_perimeter",  ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_4-route_bottom-anchilla_perimeter","shared_4-route_bottom-anchilla_perimeter", ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_4-route_upper-anchilla_perimeter", "shared_4-route_upper-anchilla_perimeter",  ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    # name                                                     config                                       extra args
+    ("no_hbm-compact_layout",                                  "no_hbm",                                   ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("no_hbm_single_magic_state-compact_layout",               "no_hbm_single_magic_state",                ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("shared_none-compact_layout",                             "shared_none",                              ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("shared_2-route_bottom-compact_layout",                   "shared_2-route_bottom",                    ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("shared_2-route_upper-compact_layout",                    "shared_2-route_upper",                     ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_4-route_bottom-compact_layout",                   "shared_4-route_bottom",                    ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_4-route_upper-compact_layout",                    "shared_4-route_upper",                     ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("shared_none-anchilla_perimeter-compact_layout",          "shared_none-anchilla_perimeter",           ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("shared_2-route_bottom-anchilla_perimeter-compact_layout","shared_2-route_bottom-anchilla_perimeter", ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    ("shared_2-route_upper-anchilla_perimeter-compact_layout", "shared_2-route_upper-anchilla_perimeter",  ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_4-route_bottom-anchilla_perimeter-compact_layout","shared_4-route_bottom-anchilla_perimeter", ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_4-route_upper-anchilla_perimeter-compact_layout", "shared_4-route_upper-anchilla_perimeter",  ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
 
-    # ("no_hbm",                                  "no_hbm",                                   ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # ("no_hbm_single_magic_state",               "no_hbm_single_magic_state",                ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_none",                             "shared_none",                              ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_2-route_bottom",                   "shared_2-route_bottom",                    ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_2-route_upper",                    "shared_2-route_upper",                     ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # # ("shared_4-route_bottom",                   "shared_4-route_bottom",                    ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # # ("shared_4-route_upper",                    "shared_4-route_upper",                     ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_none-anchilla_perimeter",          "shared_none-anchilla_perimeter",           ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_2-route_bottom-anchilla_perimeter","shared_2-route_bottom-anchilla_perimeter", ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # ("shared_2-route_upper-anchilla_perimeter", "shared_2-route_upper-anchilla_perimeter",  ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # # ("shared_4-route_bottom-anchilla_perimeter","shared_4-route_bottom-anchilla_perimeter", ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
-    # # ("shared_4-route_upper-anchilla_perimeter", "shared_4-route_upper-anchilla_perimeter",  ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("no_hbm-squared_sparse",                                  "no_hbm",                                   ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("no_hbm_single_magic_state-squared_sparse",               "no_hbm_single_magic_state",                ["-arch", "compact_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_none-squared_sparse",                             "shared_none",                              ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_2-route_bottom-squared_sparse",                   "shared_2-route_bottom",                    ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_2-route_upper-squared_sparse",                    "shared_2-route_upper",                     ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # # ("shared_4-route_bottom-squared_sparse",                   "shared_4-route_bottom",                    ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # # ("shared_4-route_upper-squared_sparse",                    "shared_4-route_upper",                     ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_none-anchilla_perimeter-squared_sparse",          "shared_none-anchilla_perimeter",           ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_2-route_bottom-anchilla_perimeter-squared_sparse","shared_2-route_bottom-anchilla_perimeter", ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # ("shared_2-route_upper-anchilla_perimeter-squared_sparse", "shared_2-route_upper-anchilla_perimeter",  ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # # ("shared_4-route_bottom-anchilla_perimeter-squared_sparse","shared_4-route_bottom-anchilla_perimeter", ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
+    # # ("shared_4-route_upper-anchilla_perimeter-squared_sparse", "shared_4-route_upper-anchilla_perimeter",  ["-arch", "square_sparse_layout", "-tmr", f"{args.tmr}"]),
 ]
 
 # === Helper functions ===
@@ -83,6 +84,22 @@ def load_steps(path):
     except:
         return None
 
+def load_footprint(path):
+    """Load max number of occupied ancilla of any timestep."""
+    with open(path, 'r') as f:
+        data = json.load(f)
+    steps = data.get("steps")
+    if steps == "timeout":
+        return None
+    max_occupied = 0
+    for timestep in data["steps"]:
+        # Sum lengths of "path" lists for all operations in this timestep
+        occupied = sum(len(op["path"]) for op in timestep)
+        print(occupied)
+        max_occupied = max(max_occupied, occupied)
+
+    return max_occupied
+
 def run_wisq_case(bench_path, bench_name, case_name, hbm_config, extra_wisq_args, run_idx):
     """Single benchmark/architecture run."""
     out_path = os.path.join(bench_output_dir, f"{bench_name}{case_name.replace(' ', '')}_run{run_idx}.out")
@@ -95,8 +112,9 @@ def run_wisq_case(bench_path, bench_name, case_name, hbm_config, extra_wisq_args
     # print(cmd)
     ok = run_and_stream(cmd, env, log_path)
     steps = load_steps(out_path) if ok else None
+    routing_footprint = load_footprint(out_path) if ok else None
 
-    return bench_name, case_name, run_idx, steps
+    return bench_name, case_name, run_idx, steps, routing_footprint
 
 # === MAIN ===
 def main():
@@ -117,34 +135,47 @@ def main():
         future_to_job = {executor.submit(run_wisq_case, *job): job for job in all_jobs}
 
         for i, future in enumerate(as_completed(future_to_job), start=1):
-            bench_name, case_name, run_idx, steps = future.result()
+            bench_name, case_name, run_idx, steps, routing_footprint = future.result()
             print(f"[{i}/{len(all_jobs)}] ✅ {bench_name} | {case_name:<8} | run {run_idx} → {steps if steps is not None else 'timeout/error'} steps")
-            results.append((bench_name, case_name, run_idx, steps))
+            results.append((bench_name, case_name, run_idx, steps, routing_footprint))
 
-    # === Summarize results ===
-    summary_path = os.path.join(bench_output_dir, "results.csv")
-    bench_modes = {}
+    # results: (bench_name, case_name, run_idx, steps, routing_footprint)
 
-    for bench, case, _, steps in results:
-        bench_modes.setdefault(bench, {}).setdefault(case, []).append(steps)
+    steps = {}
+    routing = {}
+    benches = set()
+    cases = set()
 
-    with open(summary_path, "w", newline='') as f:
-        writer = csv.writer(f)
-        # Write header
-        header = ["Benchmark"] + [case_name for case_name, _, _ in HBM_CASES]
-        writer.writerow(header)
+    for b, c, run, s, r in results:
+        benches.add(b)
+        cases.add(c)
 
-        # Write each benchmark's results
-        for bench, cases in sorted(bench_modes.items()):
-            row = [bench]
-            for case_name, _, _ in HBM_CASES:
-                vals = [v for v in cases.get(case_name, []) if v is not None]
-                avg = round(statistics.mean(vals), 1) if vals else ""
-                row.append(avg)
-            writer.writerow(row)
-            print(",".join(map(str, row)))
+        if s is not None:
+            if (b, c) not in steps:
+                steps[(b, c)] = []
+            steps[(b, c)].append(s)
 
-    print(f"\n✅ Summary saved at: {summary_path}")
+        if r is not None:
+            if (b, c) not in routing:
+                routing[(b, c)] = []
+            routing[(b, c)].append(r)
+
+    benches = sorted(benches)
+    cases = sorted(cases)
+
+    def write_csv(filename, data):
+        with open(filename, "w", newline="") as f:
+            w = csv.writer(f)
+            w.writerow(["bench_name"] + cases)
+            for b in benches:
+                row = [b]
+                for c in cases:
+                    vals = data.get((b, c), [])
+                    row.append(statistics.mean(vals) if vals else "")
+                w.writerow(row)
+
+    write_csv("num_steps.csv", steps)
+    write_csv("routing_footprint.csv", routing)
 
 if __name__ == "__main__":
     main()
